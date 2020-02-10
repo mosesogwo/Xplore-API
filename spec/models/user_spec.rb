7636a22b1015aa6300a83115f1bcce0976db7122
Moses Ogwo <mosesogwo@gmail.com>
2020-02-10 14:23:15 +0100

    add User associations test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  #Validations
-  it { should validate_presence_of(:full_name) }
-  it { should validate_presence_of(:username) }
-  it { should validate_uniqueness_of(:username) }
+  describe 'Validations' do
+    it { should validate_presence_of(:full_name) }
+    it { should validate_presence_of(:username) }
+    it { should validate_uniqueness_of(:username) }
+  end
+
+  describe 'Associations' do
+    it { should have_many(:favourites) }
+    it { should have_many(:plans).through(:favourites) }
+  end
 end
