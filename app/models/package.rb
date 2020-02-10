9aef0486a7c503faf27bff1a86b26eb2a7f1bbaa
Moses Ogwo <mosesogwo@gmail.com>
2020-02-10 14:50:52 +0100

    Package association tests passing except have_many_attached :images

--- app/models/package.rb
+++ app/models/package.rb
@@ -1,2 +1,8 @@
 class Package < ApplicationRecord
+  validates :destination, :price, :details, presence: true
+  validates :price, numericality: true
+
+  has_many :wishes
+  has_many :users, through: :wishes
+  has_many_attached :images
 end
--- spec/models/package_spec.rb
+++ spec/models/package_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Package, type: :model do
     it { should validate_presence_of(:destination) }
     it { should validate_presence_of(:price) }
     it { should validate_presence_of(:details) }
+    it { should validate_numericality_of(:price)}
   end
 
   describe 'Associations' do
