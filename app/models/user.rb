5562b1740cffe4616521979681b2e277549fce3d
Moses Ogwo <mosesogwo@gmail.com>
2020-02-10 14:17:22 +0100

    validate uniqueness of username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   validates :full_name, :username, presence: true
+  validates :username, uniqueness: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,5 @@ RSpec.describe User, type: :model do
   #Validations
   it { should validate_presence_of(:full_name) }
   it { should validate_presence_of(:username) }
+  it { should validate_uniqueness_of(:username) }
 end
