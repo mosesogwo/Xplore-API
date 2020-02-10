695347ee422f4099c6d39e11a6bad64f5793288d
Moses Ogwo <mosesogwo@gmail.com>
2020-02-10 14:12:07 +0100

    add validation test for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #Validations
+  it { should validate_presence_of(:full_name) }
+  it { should validate_presence_of(:username) }
 end
