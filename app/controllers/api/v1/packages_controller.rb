779af030d49f8d1d0ae7a19ef95fcb9c4e399b4d
Moses Ogwo <mosesogwo@gmail.com>
2020-02-11 10:53:17 +0100

    add spec/requests directory with packages_spec and wishes spec files

---
+++ app/controllers/api/v1/packages_controller.rb
@@ -0,0 +1,12 @@
+module Api
+  module V1
+    class PackagesController < ApplicationController
+
+      # def index
+      #   packages = Package.all
+      #   render json: {status: 'SUCCESS', message: 'Loaded Packages', data: packages}, status: :ok
+      # end
+
+    end
+  end
+end
