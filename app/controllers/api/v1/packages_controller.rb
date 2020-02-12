3890f7a9355ccac63403f1c2f9f88e693005c007
Moses Ogwo <mosesogwo@gmail.com>
2020-02-12 12:01:42 +0100

    create attach_images private method in Packages controller

--- app/controllers/api/v1/packages_controller.rb
+++ app/controllers/api/v1/packages_controller.rb
@@ -3,8 +3,14 @@ module Api
     class PackagesController < ApplicationController
       def index
         packages = Package.all
-        data = packages.map { |package| package.as_json.merge({ images: package.images.map { |image| ({ image: url_for(image) }) } }) }
+        data = packages.map {|package| attach_images(package)}
         render json: { status: 'SUCCESS', message: 'Loaded Packages', data: data }, status: :ok
+      end
+
+      private
+
+      def attach_images(package)
+        package.as_json.merge({ images: package.images.map { |image| ({ image: url_for(image) }) } })
       end
     end
   end
