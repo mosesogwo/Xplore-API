797368d39b0518846a3fbcef9c56a30aa35c03cc
Moses Ogwo <mosesogwo@gmail.com>
2020-02-15 03:35:49 +0100

    remove all packages from dev db

--- app/models/package.rb
+++ app/models/package.rb
@@ -2,7 +2,7 @@ class Package < ApplicationRecord
   validates :destination, :price, :details, presence: true
   validates :price, numericality: true
 
-  has_many :wishes
+  has_many :wishes, dependent: :destroy
   has_many :users, through: :wishes
-  has_many_attached :images
+  has_one_attached :image
 end
