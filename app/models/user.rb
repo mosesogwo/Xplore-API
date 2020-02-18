504ad85eab509b84eb2bc181b9f06eb0fe88e4ba
Moses Ogwo <mosesogwo@gmail.com>
2020-02-18 13:41:21 +0100

    configure GCS for cloud file storage

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   validates :username, presence: true
   validates :username, uniqueness: true
 
-  has_many :wishes
+  has_many :wishes, dependent: :destroy
   has_many :packages, through: :wishes
 end
